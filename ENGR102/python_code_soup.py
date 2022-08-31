fa967a0910475a4236f4564f1183e6190fe686bd
Ilan Sela <ilansela42@gmail.com>
2022-08-31 07:38:26 -0700

    fourth

---
+++ ENGR102/python_code_soup.py
@@ -0,0 +1,56 @@
+from re import X
+
+
+# By submitting this assignment, I agree to the following:
+#   "Aggies do not lie, cheat, or steal, or tolerate those who do."
+#   "I have not given or received any unauthorized aid on this assignment."
+#
+# Name:         Ilan Sela
+# Section:      401
+# Assignment:   Topic 2 (ind-p3)
+# Date:         24 August 2022
+
+#first num
+z=0
+x=1
+z+=x
+print(z)
+
+#second num
+x=1
+x+=1
+y=10
+y*=x
+z=0
+x+=1
+x+=1
+x+=1
+x+=1
+z+=y
+z+=x
+print(z)
+
+#third num
+y=10
+x=y
+y*=x
+x=1
+x+=1
+z=0
+z+=x
+z+=y
+print(z)
+
+#fourth num
+y=10
+x=y
+y*=x
+y*=x
+x=y
+y*=x
+y*=x
+z=0
+z+=y
+print(z)
+
+#fifth num
