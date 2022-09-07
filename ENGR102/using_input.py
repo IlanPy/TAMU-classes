8f91b0ae6cbd75c7421602b4e9019598fc5302bd
Ilan Sela <ilansela42@gmail.com>
2022-09-07 07:07:59 -0700

    trial 3

--- ENGR102/using_input.py
+++ ENGR102/using_input.py
@@ -22,18 +22,18 @@ print("Force is", force, "N")
 
 #Calculate the wavelength of x-rays scattering from a crystal lattice with a distance between 
 #crystal layers of 0.025 nm, scattering angle of 25 degrees, and first order diffraction using Bragg’s Law .
-print("This program calculates the wavelength given distance and angle")
+print("\nThis program calculates the wavelength given distance and angle")
 print("Please enter the distance (nm): ", end = "")
 d = float(input())
 print("Please enter the angle (degrees): ", end = "")
 angle = (float(input())*pi)/180
 wavelength = 2*d*sin(angle)
-print("Wavelength is", wavelength, "nm")
+print(f'Wavelength is, {wavelength:.4}, "nm')
 
 
 #Calculate how much Radon-222 is left after 3 days of radioactive decay given an initial amount 
 #of 5 g and a half-life of 3.8 days using the equation of radioactive decay.
-print("This program calculates how much radon-222 is left given time and initial amount")
+print("\nThis program calculates how much Radon-222 is left given time and initial amount")
 print("Please enter the time (days): ", end = "")
 half_life = float(input())/2
 print("Please enter the initial amount (g): ", end = "")
@@ -44,12 +44,12 @@ print("Radon-222 left is", radon_222, "g")
 
 #Calculate the pressure of 5 moles of an ideal gas with a volume of 0.25 m^3, and temperature of 
 #415 K using the Ideal Gas Law
-print("This program calculates the pressure given moles, volume, and temperature")
+print("\nThis program calculates the pressure given moles, volume, and temperature")
 print("Please enter the number of moles: ", end="")
 moles = float(input())
 print("Please enter the volume (m^3): ", end ="")
 volume = float(input())
-print("Please enter the temperature: ", end="")
+print("Please enter the temperature (K): ", end="")
 temp = float(input())
 pressure = (temp*moles*8.314)/(1000*volume)
 print("Pressure is", pressure,"kPa")
