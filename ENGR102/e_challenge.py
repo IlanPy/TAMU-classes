d108e48f585867d6ab928e411845be6ad94d6b15
Ilan Sela <ilansela42@gmail.com>
2022-09-07 12:40:27 -0700

    bonus challenge file

---
+++ ENGR102/e_challenge.py
@@ -0,0 +1,16 @@
+# By submitting this assignment, I agree to the following:
+#   "Aggies do not lie, cheat, or steal, or tolerate those who do."
+#   "I have not given or received any unauthorized aid on this assignment."
+#
+# Name:         Ilan Sela
+# Section:      401
+# Assignment:   Topic 3 challenge
+# Date:         7 September 2022
+
+from math import *
+
+print("Please enter the number of digits of precision for e: ", end="")
+input = int(input())
+
+#output string
+print(f"the value of e to {input} digits is: {e:.{input}f}")
