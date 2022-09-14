6dc8ba8fc66b42ba93e89c2e3e1dc0f1853bf5ec
Ilan Sela <ilansela42@gmail.com>
2022-09-14 10:36:36 -0700

    commiting

--- ENGR102/boolean_expressions.py
+++ ENGR102/boolean_expressions.py
@@ -11,6 +11,9 @@
 # Date:         14 September 2022
 
 ############ Part A ############ 
+from operator import truediv
+
+
 print("Enter True or False for a: ", end="")
 a = input()
 print("Enter True or False for b: ", end="")
@@ -45,3 +48,21 @@ print(f"XOR: {xor}")
 
 odd2 = (a and b and c) or ((a and not(b) and not(c)) or (not(a) and not(c) and b) or (not(b) and c and not(a)))
 print(f"Odd number: {odd2}")
+
+############ Part D ############ 
+complex1 = (not (a and not b) or (not c and b)) and (not b) or (not a and b and not c) or (a and not b) 
+complex2 = (not ((b or not c) and (not a or not c))) or (not (c or not (b and c))) or (a and not c) and (not a or (a and b 
+and c) or (a and ((b and not c) or (not b)))) 
+print(complex1)
+simple1 = (not(a and b and c)) and (a or b and c) and (a and b or c)
+print(simple1)
+'''
+if a and b and c = true: false
+if a and b = false and c = true: true
+if a and c = false and b = true: true
+if b and c = false and a = true: true
+if a and b = true and c = false: false
+if a and c = true and b = false: true
+if b and c = true and a = false: false
+if a and b and c = false: true
+'''
