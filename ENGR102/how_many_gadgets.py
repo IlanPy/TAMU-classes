7a8663d6e1938f276ee899f0303e340a45f7ae87
Ilan Sela <ilansela42@gmail.com>
2022-09-14 11:39:58 -0700

    cmomit

--- ENGR102/how_many_gadgets.py
+++ ENGR102/how_many_gadgets.py
@@ -7,21 +7,31 @@
 # Assignment:   Topic 4 Activity #4
 # Date:         14 September 2022
 gadget = 0
+gadget2=0
+gadget3=0
 print('Please enter a positive value for day: ', end = "")
 days = int(input())
+
+#if statements
 if(days<0):
     print(f'You entered an invalid number!')
 else:
     if(days<11):
         gadget = 5*days
     elif(days<61):
-        gadget = gadget + 50*days
+        gadget = 5*days
+        gadget = gadget + 50*(days- 9)
     elif(days<102):
+        gadget = 5*days
+        gadget = gadget + 50*(days-9)
         base = 101-60
         height = 50 
         area = (base*height)*0.5
         base2 = 101-days
-        height2 = 50 - (days - 60)
+        height2 = 50 - (days-60)
         area2 = (base2*height2)*0.5
         gadget = gadget + (area-area2)
+        
+        print(area2)
+        print(area+gadget)
     print(f'The total number of gadgets produced on day {days} is {gadget}')
