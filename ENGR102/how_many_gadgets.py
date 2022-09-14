199a2e4b8b1d24298c7fe35ac8b793d332b63796
Ilan Sela <ilansela42@gmail.com>
2022-09-14 11:49:54 -0700

    commit

--- ENGR102/how_many_gadgets.py
+++ ENGR102/how_many_gadgets.py
@@ -19,11 +19,11 @@ else:
     if(days<11):
         gadget = 5*days
     elif(days<61):
-        gadget = 5*days
-        gadget = gadget + 50*(days- 9)
+        gadget = 5*10
+        gadget = gadget + 50*(days- 10)
     elif(days<102):
-        gadget = 5*days
-        gadget = gadget + 50*(days-9)
+        gadget = 5*10
+        gadget = gadget + 50*(60-10)
         base = 101-60
         height = 50 
         area = (base*height)*0.5
@@ -31,7 +31,7 @@ else:
         height2 = 50 - (days-60)
         area2 = (base2*height2)*0.5
         gadget = gadget + (area-area2)
-        
+
         print(area2)
         print(area+gadget)
     print(f'The total number of gadgets produced on day {days} is {gadget}')
