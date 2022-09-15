3e5effb024f818584560f105f1c5022073737455
Ilan Sela <ilansela42@gmail.com>
2022-09-15 14:46:26 -0700

    com

--- ENGR102/calculate_roots.py
+++ ENGR102/calculate_roots.py
@@ -1,3 +1,12 @@
+# By submitting this assignment, I agree to the following:
+#   "Aggies do not lie, cheat, or steal, or tolerate those who do."
+#   "I have not given or received any unauthorized aid on this assignment."
+#
+# Name:         Ilan Sela
+# Section:      401
+# Assignment:   Topic 3 challenge
+# Date:         7 September 2022
+
 from math import sqrt
 root1=0
 print(f'Please enter the coefficient A: ', end = '')
@@ -7,15 +16,16 @@ b=float(input())
 print(f'Please enter the coefficient C: ', end = '')
 c=float(input())
 
+#loops
 det = (b**2-(4*a*c))
 if(det<0):
     root1 = (-b)/(2*a)
     complexnum = sqrt(abs(det))/2*a
-    print(f'The root is x = {root1} + {complexnum}i and x = {root1} - {complexnum}i')
+    print(f'The roots are x = {root1} + {complexnum}i and x = {root1} - {complexnum}i')
 elif (a==0):
-    root1 = -b/c
+    root1 = -c/b
     if(b==0 and a==0 and c!=0):
-        print(f'c has to equal zero if both a and b are.')
+        print(f'You entered an invalid combination of coefficients!')
     else:
         print(f'The root is x = {root1}')
 elif (a!=0):
@@ -23,8 +33,8 @@ elif (a!=0):
     root2 = ((-b-sqrt(b**2-(4*a*c)))/2*a)
     if(root1!=root2):
         if(root1>root2):
-            print(f'The root is x = {root1} and x = {root2}')
+            print(f'The roots are x = {root1} and x = {root2}')
         else:
-           print(f'The root is x = {root2} and x = {root1}') 
+           print(f'The roots are x = {root2} and x = {root1}') 
     else:
         print(f'The root is x = {root1}')
