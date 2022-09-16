01a2b063c54a88dace9a7bd7e6649fa6fb6c95fb
Ilan Sela <ilansela42@gmail.com>
2022-09-15 20:12:03 -0700

    3

--- ENGR102/how_many_gadgets.py
+++ ENGR102/how_many_gadgets.py
@@ -25,6 +25,6 @@ else:
     elif(days<101):
         gadget = 10*5 + 50*50 + ((days-60)*50 - ((days-60)*(days-60+1))/2)
     elif(days>100):
-        gadget = 10*5 + 50*50 + ((101-60)*50 - ((101-60)*(101-60+1))/2)
+        gadget = 10*5 + 50*50 + ((100-60)*50 - ((100-60)*(100-60+1))/2)
 
     print(f'The total number of gadgets produced on day {days} is {int(gadget)}')
