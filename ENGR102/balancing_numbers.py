4879ac0164fe8dcae44c3a5944671cb4f39f0a74
Ilan Sela <ilansela42@gmail.com>
2022-09-28 07:28:46 -0700

    s

--- ENGR102/balancing_numbers.py
+++ ENGR102/balancing_numbers.py
@@ -12,9 +12,6 @@ from math import *
 print(f'Enter a value for n: ', end='')
 n = int(input())
 sum = 0
-j = 0 
-i= n+1
-count=0
 r=0
 sum2=0
 
@@ -24,9 +21,9 @@ for i in range(1,n):
 
 r=int((-2*n-1+sqrt(8*(n**2)+1))/2)
 
+#for loop
 for i in range (n+1,n+r+1):
     sum2+=i
-    print(i)
 if sum2==sum:
     print(f'{n} is a balancing number with r={r}')
 else:
