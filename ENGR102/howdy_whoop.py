1648c42043619427d8c8ecb16e5e044f6306cf43
Ilan Sela <ilansela42@gmail.com>
2022-09-28 06:14:59 -0700

    s

--- ENGR102/howdy_whoop.py
+++ ENGR102/howdy_whoop.py
@@ -7,17 +7,26 @@
 # Assignment:   Lab 6 Infividual
 # Date:         24 August 2022
 
+#input
 print(f'Enter an integer: ', end='')
 num1 = int(input())
 print(f'Enter another integer: ', end='')
 num2 = int(input())
 
+#for loop testing values
 for i in range(1,101):
+    #first
     if(i%num1==0 and i%num2!=0):
         print(f'Howdy')
+
+    #second
     elif(i%num2==0 and i%num1!=0):
         print(f'Whoop')
+
+    #third
     elif(i%num1==0 and i%num2==0):
         print(f'Howdy Whoop')
+
+    #else
     else:
         print(i)
