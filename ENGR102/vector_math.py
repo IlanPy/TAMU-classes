5fe9b7b31706f50a4ef74c30aa470c79f1abea32
Ilan Sela <ilansela42@gmail.com>
2022-10-05 14:54:50 -0700

    first commit

---
+++ ENGR102/vector_math.py
@@ -0,0 +1,57 @@
+# By submitting this assignment, I agree to the following:
+#   "Aggies do not lie, cheat, or steal, or tolerate those who do."
+#   "I have not given or received any unauthorized aid on this assignment."
+#
+# Name:         Ilan Sela
+# Section:      401
+# Assignment:   Lab 7b
+# Date:         5 October 2022
+from math import *
+
+print('Enter the elements for vector A: ', end='')
+strA = input()
+lA = strA.split(" ")
+print('Enter the elements for vector B: ', end='')
+strB = input()
+lB = strB.split(" ")
+listA=[]
+listB=[]
+magA = 0
+magB = 0
+dotprod = 0
+product = []
+listAdd = []
+listSub = []
+
+for i in lA:
+    listA.append(float(i))
+
+for i in listA:
+    magA+=(i**2)
+magA=sqrt(magA)
+
+for i in lB:
+    listB.append(float(i))
+
+for i in listB:
+    magB+=(i**2)
+magB=sqrt(magB)
+
+
+for i in range(len(listA)):
+    listAdd.append(listA[i] + listB[i])
+for i in range(len(listB)):
+    listSub.append(listA[i] - listB[i])
+
+
+for i in range(len(listA)):
+    product.append(listA[i] * listB[i])
+for i in range(len(product)):
+    dotprod+=product[i]
+
+print(f'The magnitude of vector A is {magA:.5f}')
+print(f'The magnitude of vector B is {magB:.5f}')
+print(f'A + B is {listAdd}')
+print(f'A - B is {listSub}')
+print(f'The dot product is {dotprod:.2f}')
+
