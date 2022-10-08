104297d250a0afed6ff361f321e651746e3cb8d3
Ilan Sela <ilansela42@gmail.com>
2022-10-07 20:19:13 -0700

    simplified

--- ENGR102/kaprekars_constant.py
+++ ENGR102/kaprekars_constant.py
@@ -9,8 +9,6 @@
 
 print('Enter a four-digit integer: ', end ='')
 digit = input()
-mylist = []
-revlist = []
 mystr = ''
 revstr = ''
 store = [digit]
@@ -18,39 +16,22 @@ store = [digit]
 while(len(digit)<4):
     digit = '0' + digit
 
-for i in digit:
-    mylist.append(i)
+mylist = list(digit)
 
-for i in digit:
-    revlist.append(i)
+revlist = list(mylist)
 
 #repeated case
-if(mylist[0]==mylist[1]==mylist[2]==mylist[3]):
+if(len(set(mylist)) == 1):
     print(f'{digit} > 0\n{digit} reaches 0 via Kaprekar\'s routine in 1 iterations')
     exit()
 
 for i in range(8):
     #convert to int list
-    s=0
-    for i in mylist:
-        mylist[s]= int(i)
-        s+=1
-    s=0
-    for i in revlist:
-        revlist[s]=int(i)
-        s+=1
+
     #sort int list
     mylist.sort()
     revlist.sort(reverse=True)
-    #conert to string list
-    s=0
-    for i in mylist:
-        mylist[s]=str(i)
-        s+=1
-    s=0
-    for i in revlist:
-        revlist[s]=str(i)
-        s+=1
+
     #join list into a str
     mystr = "".join(mylist)
     revstr = "".join(revlist)
@@ -58,30 +39,18 @@ for i in range(8):
     digit = int(mystr)
     revdigit = int(revstr)
     digit = revdigit - digit
+
     #convert int to string
     digit = str(digit)
     #store a list of digits
     store.append(digit)
-    while(len(digit)<4):
-        digit = '0' + digit
+    digit = '0' * (4-len(digit)) + digit
     #convert string to list
-    s=0
-    for i in digit:
-        mylist[s]=i
-        s+=1
-    s=0
-    for i in digit:
-        revlist[s]=i
-        s+=1
+    mylist = list(digit)
+    revlist = list(mylist)
     if(digit == '6174'):
         break
 
 #print
-s=1
-for i in store:
-    print(f'{i}', end = '')
-    if(s<len(store)):
-        print(f' > ', end = '')
-    s+=1
-
-print(f'\n{store[0]} reaches 6174 via Kaprekar\'s routine in {len(store)-1} iterations')
+print(" > ".join(store))
+print(f'{store[0]} reaches 6174 via Kaprekar\'s routine in {len(store)-1} iterations')
