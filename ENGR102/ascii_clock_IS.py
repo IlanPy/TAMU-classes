dc263ffb9057134c2ac58a6b5e9c37de1f141d33
Ilan Sela <ilansela42@gmail.com>
2022-10-17 05:51:36 -0700

    completed loop

---
+++ ENGR102/ascii_clock_IS.py
@@ -0,0 +1,47 @@
+# By submitting this assignment, I agree to the following:
+#   "Aggies do not lie, cheat, or steal, or tolerate those who do."
+#   "I have not given or received any unauthorized aid on this assignment."
+#
+# Names:        Jordan Rivas
+#               Ilan Sela
+#               Robert Stacks
+#               Dave Wright
+# Section:      501
+# Assignment:   LAB Topic 8 Team: ASCII Clock
+# Date:         12 October 2022
+
+# (Jordan) Get user input for clock time; store in variable "user_input" 
+
+# (Jordan) Convert input to list; store in variable "input_as_list"
+
+
+# ASCII Template Definitions
+# (Jordan) Define master_list as empty list; will contain the 5 lists defined below
+
+# (Robert) List line1 containing the first line of the ASCII form for each number/colon; append the list to master_list
+
+# (Robert) List line2 containing the second line of the ASCII form for each number/colon; append the list to master_list
+
+# (Robert) List line3 containing the third line of the ASCII form for each number/colon; append the list to master_list
+
+# (Dave) List line4 containing the fourth line of the ASCII form for each number/colon; append the list to master_list
+
+# (Dave) List line5 containing the fifth line of the ASCII form for each number/colon; append the list to master_list
+
+
+
+# For loop that runs 5 times to print out each number/colon
+# (Ilan) Outer loop header
+for i in range(5):
+    # (Ilan) Inner loop header - runs for every element in input_as_list
+    for access_number in input_as_list:
+        # (Ilan) Access input_as_list at index corresponding to loop control variable and convert that element to int; store in "access_number"
+        # If the accessed element is a colon, assign 10 to variable "access_number"
+        if(access_number == ':'):
+            access_number = 10
+        # (Ilan) Access master_list at index corresponding to outer loop control variable
+        # At list accessed in master_list, access element at access_number and print it followed by space
+        print(f'{master_list[i][access_number]} ')
+    # Print new line
+    print('\n')
+
---
+++ ENGR102/kaprekars_challenge.py
@@ -0,0 +1,54 @@
+# By submitting this assignment, I agree to the following:
+#   "Aggies do not lie, cheat, or steal, or tolerate those who do."
+#   "I have not given or received any unauthorized aid on this assignment."
+#
+# Name:         Ilan Sela
+# Section:      401
+# Assignment:   Lab 7c
+# Date:         5 October 2022
+
+mystr = ''
+revstr = ''
+store = [digit]
+
+while(len(digit)<4):
+    digit = '0' + digit
+
+mylist = list(digit)
+
+revlist = list(mylist)
+
+#repeated case
+if(len(set(mylist)) == 1):
+    print(f'{digit} > 0\n{digit} reaches 0 via Kaprekar\'s routine in 1 iterations')
+    exit()
+
+for i in range(8):
+    #convert to int list
+
+    #sort int list
+    mylist.sort()
+    revlist.sort(reverse=True)
+
+    #join list into a str
+    mystr = "".join(mylist)
+    revstr = "".join(revlist)
+    #convert string into an int
+    digit = int(mystr)
+    revdigit = int(revstr)
+    digit = revdigit - digit
+
+    #convert int to string
+    digit = str(digit)
+    #store a list of digits
+    store.append(digit)
+    digit = '0' * (4-len(digit)) + digit
+    #convert string to list
+    mylist = list(digit)
+    revlist = list(mylist)
+    if(digit == '6174'):
+        break
+
+#print
+print(" > ".join(store))
+print(f'{store[0]} reaches 6174 via Kaprekar\'s routine in {len(store)-1} iterations')
