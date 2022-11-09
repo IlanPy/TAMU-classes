83dfb3aab50379c6b9186ef0fce2c73a0fb8dffb
Ilan Sela <ilansela42@gmail.com>
2022-11-09 07:27:43 -0800

    add comments

--- ENGR102/barcode_checker.py
+++ ENGR102/barcode_checker.py
@@ -7,18 +7,23 @@
 # Assignment:   Lab 11 indv #1
 # Date:         9 November 2022
 
+#open file
 with open('barcodes.txt', 'r') as myfile:
+    #set valid counter
     valid = 0
+    #loop each line
     for new_line in myfile:
         mylist = list(new_line)
         if '\n' in mylist:
             mylist.remove('\n')
         mylist.pop(12)
+        #int each element
         for s in range(len(mylist)):
             mylist[s] = int(mylist[s])
         length = len(mylist)//2
         mylist1=[]
         mylist2=[]
+        #split into two lists
         for i in range(0,11,2):
             mylist1.append(mylist[i])
             mylist2.append(mylist[i+1])
@@ -28,6 +33,7 @@ with open('barcodes.txt', 'r') as myfile:
         add = sum1+multiply
         lastdigit = add%10
         subtract = 10-lastdigit
+        #check if valid
         if(subtract == (int(new_line)%10)):
             valid += 1
     print(f'There are {valid} valid barcodes')
