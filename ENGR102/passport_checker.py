088f383f10132c0ae93d905e602ac80af6444224
Ilan Sela <ilansela42@gmail.com>
2022-11-09 06:35:02 -0800

    add comments

--- ENGR102/passport_checker.py
+++ ENGR102/passport_checker.py
@@ -7,13 +7,17 @@
 # Assignment:   Lab 11 team #1
 # Date:         9 November 2022
 
+#input
 print('Enter the name of the file: ', end='')
 file_name = input()
 valid = 0
+#open file
 with open(file_name, "r") as myfile:
     for i in myfile:
+        #check if all info is given
         if('byr' in i and 'iyr' in i and 'eyr' in i and 'hgt' in i and 'ecl' in i and 'pid' in i and 'cid' in i):
             valid +=1
+    #output
     print(f'There are {valid} valid passports')
 
 
