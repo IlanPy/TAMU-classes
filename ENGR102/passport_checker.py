64f05e31c21cf20175a66c20c10d194b0311c9e4
Ilan Sela <ilansela42@gmail.com>
2022-11-09 06:17:34 -0800

    first commit

---
+++ ENGR102/passport_checker.py
@@ -0,0 +1,19 @@
+# By submitting this assignment, I agree to the following:
+#   "Aggies do not lie, cheat, or steal, or tolerate those who do."
+#   "I have not given or received any unauthorized aid on this assignment."
+#
+# Name:         Ilan Sela
+# Section:      401
+# Assignment:   Lab 11 team #1
+# Date:         9 November 2022
+
+print('Enter the name of the file: ', end='')
+file_name = input()
+with open(file_name, "r") as infile:
+    file_aslist = list(file_name)
+    valid = 0
+    for i in file_aslist:
+        if('byr' in i and 'iyr' in i and 'eyr' in i and 'hgt' in i and 'ecl' in i and 'pid' in i and 'cid' in i):
+            valid +=1
+        
+
