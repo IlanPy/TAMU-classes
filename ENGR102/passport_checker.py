17c010922495764de062ab3f50bd253284b3e384
Ilan Sela <ilansela42@gmail.com>
2022-11-09 19:41:17 -0800

    first commit

--- ENGR102/passport_checker.py
+++ ENGR102/passport_checker.py
@@ -7,23 +7,31 @@
 # Assignment:   Lab 11 team #1
 # Date:         9 November 2022
 
-#input
-print('Enter the name of the file: ', end='')
-file_name = input()
+#input file name
+file_name = input('Enter the name of the file: ')
+
+#open files
+myfile = open(file_name, "r")
+writefile = open('valid_passports.txt', "w")
+content = myfile.readlines()
+newlist = []
 valid = 0
-mylist = []
-#open file
-with open(file_name, "r") as myfile:
-    for i in myfile:
-        #check if all info is given
-        if('byr' in i and 'iyr' in i and 'eyr' in i and 'hgt' in i and 'ecl' in i and 'pid' in i and 'cid' in i):
-            valid +=1
-            mylist.append(i)
-    with open('valid_passports.txt', 'w') as infile:
-        for i in mylist:
-            infile.write(i)
-            infile.write('\n\n')
-    #output
-    print('There are ??? valid passports')
 
+#loop to make a list with each passport per line
+s=0
+for i in range(len(content)):
+    if content[i] == '\n':
+        newlist.append(''.join(content[s:i]))
+        s=i
 
+#loop to check if valid and write
+for i in newlist:
+    if('byr' in i and 'iyr' in i and 'eyr' in i and 'hgt' in i and 'ecl' in i and 'pid' in i and 'cid' in i):
+        valid+=1
+        writefile.write(i)
+        writefile.write('\n')
+
+#print and close    
+print(f'There are {valid} valid passports')
+myfile.close()
+writefile.close()
