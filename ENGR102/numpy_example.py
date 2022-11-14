1dd1d060292f60ec6430a13676334ef0a990ef1a
Ilan Sela <ilansela42@gmail.com>
2022-11-14 07:51:46 -0800

    changes

--- ENGR102/numpy_example.py
+++ ENGR102/numpy_example.py
@@ -2,9 +2,9 @@
 # tutorial, and each team member understands the material 
 import numpy as np
 
-A = np.arrange(12).reshape(3,4)
-B = np.arrange(8).reshape(4,2)
-C = np.arrange(6).reshape(2,3)
+A = np.arange(12).reshape(3,4)
+B = np.arange(8).reshape(4,2)
+C = np.arange(6).reshape(2,3)
 
 print(f'A = {A}\n\nB = {B}\n\n C = {C}\n')
 
@@ -16,6 +16,6 @@ DT = np.transpose(D)
 
 print(f'D^T = {DT}\n')
 
-E = np.sqrt(D/DT)
+E = np.sqrt(D/2)
 
 print(f'E = {E}')
